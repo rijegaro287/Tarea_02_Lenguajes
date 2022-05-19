51918766363de44e1d86653de0e437b08ec732d5
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-05-19 14:36:43 -0600

    add: reglas de velocidad, emergencia y ocup de pst

--- Hechos.pl
+++ Hechos.pl
@@ -1,3 +1,6 @@
+:- dynamic(ocupada/1).
+ocupada().
+
 %es_aeronave: aeronave
 es_aeronave(cessna).
 es_aeronave(ceechcraft).
@@ -14,6 +17,11 @@ es_aeronave(airBusA380).
 %es_pista(p2OE).
 %es_pista(p3).
 
+%vecocidad_segun_tamanio: velocidad en km/h, tamanio
+velocidad_segun_tamanio(280, pequenio).
+velocidad_segun_tamanio(230, mediano).
+velocidad_segun_tamanio(200, grande).
+
 %pista_para: pista, tamnio de aeronave que la usa
 pista_para(p1, pequenio).
 pista_para(p2_1, mediano).
@@ -23,8 +31,6 @@ pista_para(p3, grande).
 direccion(p2_1, eo).
 direccion(p2_2, oe).
 
-% pistas_ocupada(p1).
-ocupada(r).
 
 %tamanio: aeronave, tamanio
 tamanio(cessna, pequenio).
@@ -37,6 +43,7 @@ tamanio(boing747, grande).
 tamanio(airBusA340, grande).
 tamanio(airBusA380, grande).
 
+%nivel_de_tamanio: tamanio, magnitud
 nivel_de_tamanio(pequenio, 1).
 nivel_de_tamanio(mediano, 2).
 nivel_de_tamanio(grande, 3).
@@ -48,26 +55,47 @@ es_emergencia(problema_de_salud).
 es_emergencia(parto).
 
 %atiende_emergecia: personal
-atiende_emergencia(bomberos).
-atiende_emergencia(medico).
-atiende_emergencia(oij).
-atiende_emergencia(gerente_de_aerolinea).
+atiende_emergencia(medico, parto).
+atiende_emergencia(medico, problema_de_salud).
+atiende_emergencia(oij, secuestro).
+atiende_emergencia(mecanico, problema_en_el_motor).
+atiende_emergencia(bomberos, problema_en_el_motor).
 
+% :- dynamic location/1.
+location(tortuga, rio).
+location(lapa, cielo).
+location(murcielago, cueva).
+    
 %orden: usar_pista: aeronave, pista
 usar_pista(X,Y):-
-    es_aeronave(X),tamanio(X,Z),pista_para(Y,W),
-    nivel_de_tamanio(Z,M), nivel_de_tamanio(W,N),N>=M.
-    ocupada(Y).
+    es_aeronave(X),tamanio(X,Z),
+    pista_para(Y,W),
+    nivel_de_tamanio(Z,M), 
+    nivel_de_tamanio(W,N),
+    N>=M,
+    not(ocupada(Y)),
+    add_to_ocupada(Y), !.
 
-% verificar_direccion(X,Y):-
+%add_to_ocupada: pista
+add_to_ocupada(X):-
+    asserta(ocupada(X)).
 
-%Intento de mierda
-% ocupar(Y,Z):-
-%     pista_para(Y,_),
-%     pistas_ocupadas(X),
-%     append([Y],X,Z).
+%velocidad_de_aterrizaje: aeronave, velocidad
+velocidad_de_aterrizaje(X,V):-
+    es_aeronave(X),
+    tamanio(X,T),
+    velocidad_segun_tamanio(V,T).
 
+%pedir_ayuda: emergencia, llamar a
+pedir_ayuda(X,Y):-
+    es_emergencia(X),
+    atiende_emergencia(Y,X).
 
+%mayday: aeronave, pista asignada
+mayday(X,Y):-
+    es_aeronave(X),tamanio(X,Z),
+    pista_para(Y,W),
+    nivel_de_tamanio(Z,M), 
+    nivel_de_tamanio(W,N),
+    N>=M, !.
 
-%mayday(X,Y):-
-%    es_aeronave(X),tamanio(X,Z),pista_para(Y,Z).
