369729072d38de47c310fc64cbe411c63cc2af14
Ricardo Gatgens <gatgens48@gmail.com>
2022-05-24 12:31:18 -0600

    Responder a saludos

--- chatBot.pl
+++ chatBot.pl
@@ -1,24 +1,20 @@
-:-include('diccionario2.pl').
-%:-include('respuestas.pl').
+:-include('respuestas.pl').
 
 bnf(O, R):-oracion(O,[]), analizarMensaje(O, R).
 bnf(O):-not(oracion(O,[])), write('No he logrado comprender tu mensaje. Me lo podrias repetir?').
 
-analizarMensaje([hola], 'Hola! En que te puedo ayudar?').
-analizarMensaje(['si'], 'Entendido.').
+% analizarMensaje([hola], 'Hola! En que te puedo ayudar?').
+% analizarMensaje(['si'], 'Entendido.').
 
 maceyAux:-
-    write('Usuario: '),
+    % write('Usuario: '),
     read(X), 
     split_string(X, " ", ",", Y),
-    write('Usted ha escrito: '),
-    write(Y),
     write('\n'),
-    %bnf(Y, Z),
-    analizarMensaje(Y, R).
-    write('MaCEy: '), write(R),
+    bnf(Y, Z),
+    write(Z).
     write('\n'),
-    maceyAux. 
+    maceyAux.
 
 
 macey:- write('Bienvenido a MaCEy! digite su mensaje a continuacion: \n'),
--- respuestas.pl
+++ respuestas.pl
@@ -1,78 +1,82 @@
-analizarMensaje('Hola a', 'Hola! En que le puedo ayudar?').
+:-include('diccionario2.pl').
 
-%analizarMensaje(oracion(X,_), 'uwu').
+analizarMensaje(O, 'Hola! En que le puedo ayudar?'):- saludo([X], []),  miembro(X, O), !.
 
 
+analizarMensaje(O, 'No he logrado comprender tu mensaje. Me lo podrias repetir?').
 
-% solicito permiso para despegar
-% % Por favor indique el numero de vuelo.
-analizarMensaje('solicito permiso para despegar', 'Por favor indique el numero de vuelo.').
+miembro(O, [O | _]).
+miembro(O, [_ | T]):- miembro(O, T).
 
-% Numero de vuelo
-% % Por favor indique la aerolinea.
-analizarMensaje('', 'Por favor indique la aerolinea.').
+% % solicito permiso para despegar
+% % % Por favor indique el numero de vuelo.
+% analizarMensaje('solicito permiso para despegar', 'Por favor indique el numero de vuelo.').
 
-% Aerolinea 
-% % Por favor indique la matricula.
-analizarMensaje(_, 'Por favor indique la matricula.').
+% % Numero de vuelo
+% % % Por favor indique la aerolinea.
+% analizarMensaje('', 'Por favor indique la aerolinea.').
 
-% Matricula.
-% % Por favor indique el tipo de aeronave.
-analizarMensaje(_, 'Por favor indique el tipo de aeronave.').
+% % Aerolinea 
+% % % Por favor indique la matricula.
+% analizarMensaje(_, 'Por favor indique la matricula.').
 
-% Aeronave.
-% % Por favor indique la hora de salida. 
-analizarMensaje(_, 'Por favor indique la hora de salida.').
+% % Matricula.
+% % % Por favor indique el tipo de aeronave.
+% analizarMensaje(_, 'Por favor indique el tipo de aeronave.').
 
-% Hora de salida.
-% % Por favor indique la dirección de salida. 
-analizarMensaje(_, 'Por favor indique la dirección de salida.').
+% % Aeronave.
+% % % Por favor indique la hora de salida. 
+% analizarMensaje(_, 'Por favor indique la hora de salida.').
 
-% Dirección.
+% % Hora de salida.
+% % % Por favor indique la dirección de salida. 
+% analizarMensaje(_, 'Por favor indique la dirección de salida.').
+
+% % Dirección.
+% % % Se asignó _ por 5 minutos.
+% analizarMensaje(_, 'Se asignó _ por 5 minutos.').
+
+% % Gracias.
+% % % Con gusto. 
+% analizarMensaje('gracias', 'Con gusto.').
+
+% % solicito permiso para aterrizar.
+% % % Por favor indique su matricula. 
+% analizarMensaje(_, 'Por favor indique su matricula.').
+
+% % Matricula.
+% % % Por favor indique el tipo de aeronave. 
+% analizarMensaje(_, 'Por favor indique el tipo de aeronave.').
+
+% % Aeronave. 
+% % % Por favor indique su velocidad. 
+% analizarMensaje(_, 'Por favor indique su velocidad.').
+
+% % Velocidad. 
+% % % Por favor indique su distancia a la pista. 
+% analizarMensaje(_, 'Por favor indique su distancia a la pista. ').
+
+% % Distancia. 
+% % % Por favor indique su dirección. 
+% analizarMensaje(_, 'Por favor indique su dirección. ').
+
+% % Dirección.
 % % Se asignó _ por 5 minutos.
-analizarMensaje(_, 'Se asignó _ por 5 minutos.').
 
-% Gracias.
-% % Con gusto. 
-analizarMensaje('gracias', 'Con gusto.').
+% % mayday. 
+% % Por favor indique su emergencia.
+% analizarMensaje('mayday', 'Por favor indique su emergencia.').
 
-% solicito permiso para aterrizar.
-% % Por favor indique su matricula. 
-analizarMensaje(_, 'Por favor indique su matricula.').
+% % emergencia. 
+% % % Por favor identifiquese. 
+% analizarMensaje(_, 'Por favor identifiquese.').
 
-% Matricula.
-% % Por favor indique el tipo de aeronave. 
-analizarMensaje(_, 'Por favor indique el tipo de aeronave.').
-
-% Aeronave. 
-% % Por favor indique su velocidad. 
-analizarMensaje(_, 'Por favor indique su velocidad.').
-
-% Velocidad. 
-% % Por favor indique su distancia a la pista. 
-analizarMensaje(_, 'Por favor indique su distancia a la pista. ').
-
-% Distancia. 
-% % Por favor indique su dirección. 
-analizarMensaje(_, 'Por favor indique su dirección. ').
-
-% Dirección.
-% Se asignó _ por 5 minutos.
-
-% mayday. 
-% Por favor indique su emergencia.
-analizarMensaje('mayday', 'Por favor indique su emergencia.').
-
-% emergencia. 
-% % Por favor identifiquese. 
-analizarMensaje(_, 'Por favor identifiquese.').
-
-% identificación. 
-% % Se asignó _ y se envió equipo de ayuda. 
-analizarMensaje(_, 'Se asignó _ y se envió equipo de ayuda.').
+% % identificación. 
+% % % Se asignó _ y se envió equipo de ayuda. 
+% analizarMensaje(_, 'Se asignó _ y se envió equipo de ayuda.').
 
 
-% bleep bloop
-% % No he logrado comprender tu mensaje. Me lo podrias repetir?
-analizarMensaje(_, 'No he logrado comprender tu mensaje. Me lo podrias repetir?').
+% % bleep bloop
+% % % No he logrado comprender tu mensaje. Me lo podrias repetir?
+% analizarMensaje(_, 'No he logrado comprender tu mensaje. Me lo podrias repetir?').
 
